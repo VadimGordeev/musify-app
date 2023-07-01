c56be5a284998c5377f74d78e3da3b385c26f304
VadimGordeev <vadimgordeev22@gmail.com>
2023-07-01 19:10:27 +0300

    feat: add user slice, add player, edit login

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,7 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { MainSection } from '../features/MainSection/MainSection';
 import { MainLayout } from '../layouts/MainLayout/MainLayout';
-import { FirstPage } from '../pages/FirstPage/FirstPage';
+import { Auth } from '../pages/auth/auth';
 import { Loader } from '../shared/ui/Loader/Loader';
 import { useAppDispatch } from '../store/store.types';
 import { fetchUser } from '../store/user/user.api';
@@ -17,20 +17,20 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: MainSection
-      },
-      {
-        path: '/loader',
-        Component: Loader
       }
     ]
   },
   {
-    path: '/test',
-    Component: FirstPage
-  },
-  {
     path: '*',
     element: <div>Page not found</div>
+  },
+  {
+    path: '/auth',
+    Component: Auth
+  },
+  {
+    path: '/loader',
+    Component: Loader
   }
 ]);
 
